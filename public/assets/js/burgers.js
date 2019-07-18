9b7d51bc2f4f46fdc6ae654f5dfaa0ca9796d34f
Herbert Campos <wohoforever@yahoo.com>
2019-07-18 16:25:26 -0700

    implemented javascript file to make buttons work

---
+++ public/assets/js/burgers.js
@@ -0,0 +1,33 @@
+$(function() {
+    $(".devour-burger").on("click", function(event) {
+        var id = $(this).data("id");
+
+        $.ajax("/api/burgers/" + id, {
+            type: "PUT"
+        }).then(
+            function() {
+                console.log("Burger eaten with id: " + id);
+                location.reload();
+            }
+        );
+    });
+
+    $(".create-form").on("submit", function(event) {
+        event.preventDefault();
+
+        var name = $("#burger-name").val().trim();
+        if(name.length > 0) {
+            $.ajax("/api/burgers/", {
+                type: "POST",
+                data: {
+                    burger_name: name
+                }
+            }).then(
+                function() {
+                    console.log("Added "+ name + " to burger list!");
+                    location.reload();
+                }
+            );
+        }
+    });
+});
