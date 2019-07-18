1dcafb5ecb894a273ae7446a2420644977071cf2
Herbert Campos <wohoforever@yahoo.com>
2019-07-17 20:30:24 -0700

    set up the database

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ('Slider');
+INSERT INTO burgers (burger_name) VALUES ('Slamvich');
+INSERT INTO burgers (burger_name) VALUES ('Double Trouble');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Drowner', true);
