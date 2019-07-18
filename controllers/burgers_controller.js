213af3f78e8b42957fe5bd6dc24a2564ac01592f
Herbert Campos <wohoforever@yahoo.com>
2019-07-18 00:41:57 -0700

    created the controllers directory along with the burgers_controller.js file. said file code written to 3 routes and each route displays a certain function carried over from burgers.js

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,33 @@
+var express = require("express");
+var burger = require("../models/burger.js");
+
+var router = express.Router();
+
+router.get("/", function(req, res) {
+    burger.selectAll(function(data) {
+        var hbsObject = {
+            burgers: data
+        };
+        console.log(hbsObject);
+        res.render("index", hbsObject);
+    });
+});
+
+router.post("/api/burgers", function(req, res) {
+    burger.insertOne(["name"], [req.body.burger_name], function(result) {
+        res.json({
+            id: result.insertId
+        });
+    });
+});
+
+router.put("/api/burgers/:id", function(req, res) {
+    burger.updateOne(req.params.id, function(result) {
+        if (result.changedRows === 0) {
+            return res.status(404).end();
+        }
+        res.status(200).end();
+    });
+});
+
+module.exports = router;
