d46fa18281c0cabb91cfa611ac34e97308e820d1
Herbert Campos <wohoforever@yahoo.com>
2019-09-01 19:45:36 -0700

    changed vars to consts and function to arrow functions in controller js

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,10 +1,10 @@
-var express = require("express");
-var burger = require("../models/burger.js");
+const express = require("express");
+const burger = require("../models/burger.js");
 
-var router = express.Router();
+const router = express.Router();
 
-router.get("/", function(req, res) {
-    burger.selectAll(function(data) {
+router.get("/", (req, res) => {
+    burger.selectAll( data => {
         var hbsObject = {
             burgers: data
         };
@@ -13,15 +13,15 @@ router.get("/", function(req, res) {
     });
 });
 
-router.post("/api/burgers", function(req, res) {
-    burger.insertOne(["name"], [req.body.burger_name], function(result) {
+router.post("/api/burgers", (req, res) => {
+    burger.insertOne(["name"], [req.body.burger_name], () => {
         console.log("adding burger: " + req.body.burger_name);
         res.status(200).end();
     });
 });
 
-router.put("/api/burgers/:id", function(req, res) {
-    burger.updateOne(req.params.id, function(result) {
+router.put("/api/burgers/:id", (req, res) => {
+    burger.updateOne(req.params.id, result => {
         if (result.changedRows === 0) {
             return res.status(404).end();
         }
